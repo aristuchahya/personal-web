24179624b8bc3d07246bc43946d2593c2a9f1333
aristu09 <namikazeuzumaki43@gmail.com>
2024-04-17 14:21:24 +0700

    OOP-concept

---
+++ Day-2/Task5/assets/js/testimonial.js
@@ -0,0 +1,49 @@
+class Testimonial {
+  image = "";
+  quote = "";
+  author = "";
+
+  constructor(image, quote, author) {
+    this.image = image;
+    this.quote = quote;
+    this.author = author;
+  }
+
+  html() {
+    return `<div class="card-testi">
+          <img
+            src="${this.image}"
+          />
+          <p class="quote"><i>"${this.quote}"</i></p>
+          <p class="author">- ${this.author}</p>
+        </div>`;
+  }
+}
+
+const testimonial1 = new Testimonial(
+  "https://images.unsplash.com/photo-1568602471122-7832951cc4c5?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "Mantap sekali jasanya",
+  "Surya Elidanto"
+);
+
+const testimonial2 = new Testimonial(
+  "https://images.unsplash.com/photo-1557862921-37829c790f19?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "Keren lah pokoknya",
+  "Rangga Putra"
+);
+
+const testimonial3 = new Testimonial(
+  "https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?q=80&w=1476&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "Wuhuu kerenlah!",
+  "Yahya Kurniawan"
+);
+
+const testimonials = [testimonial1, testimonial2, testimonial3];
+
+let testimonialHTML = ``;
+
+for (let i = 0; i < testimonials.length; i++) {
+  testimonialHTML += testimonials[i].html();
+}
+
+document.getElementById("testi").innerHTML = testimonialHTML;
